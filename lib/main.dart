87b232d8dd2866872260dd14b784a808e72cffeb
Harlley <harlleybastos@hotmail.com>
2021-06-23 20:38:01 -0300

    dev:first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
